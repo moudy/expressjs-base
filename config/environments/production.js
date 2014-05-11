e7d343dc949bbde48e1523f4adbb43100ec55c3b
Moudy <moudy.elkammash@gmail.com>
2014-05-11 17:22:37 -0400

    try cloudfront

--- config/environments/production.js
+++ config/environments/production.js
@@ -1,7 +1,7 @@
 module.exports = function (app) {
   var env = process.env;
   app.set('port', env.PORT);
-  app.set('ASSET_HOST', '//s3.amazonaws.com/expressjs-base');
+  app.set('ASSET_HOST', '//dk2xqe978zxxy.cloudfront.net');
 
   console.warn('Configure "%s"', __filename);
   // app.set('MONGO_URI', env.MONGOLAB_URI);
