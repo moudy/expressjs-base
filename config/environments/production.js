d1c7326c00325f5e2ce51bdcab5735f1ab25053c
Moudy <moudy.elkammash@gmail.com>
2014-05-11 16:37:14 -0400

    asset host

--- config/environments/production.js
+++ config/environments/production.js
@@ -1,6 +1,7 @@
 module.exports = function (app) {
   var env = process.env;
   app.set('port', env.PORT);
+  app.set('ASSET_HOST', '//s3.amazonaws.com/expressjs-base');
 
   console.warn('Configure "%s"', __filename);
   // app.set('MONGO_URI', env.MONGOLAB_URI);
