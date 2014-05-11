14170a8dbf8ee0f31085ddc4f786d667ae4cb211
Moudy <moudy.elkammash@gmail.com>
2014-05-11 15:05:21 -0400

    fix asset rack

--- config/middleware/assets-production.js
+++ config/middleware/assets-production.js
@@ -1,30 +1,14 @@
-var fs = require('fs');
 var path = require('path');
 var rack = require('asset-rack');
-var brocfile = require('../../Brocfile');
-var mime = require('mime');
 
-var StaticAsset = rack.Asset.extend({
-  create: function(options) {
-    this.filename = path.resolve(options.filename);
-    if (!this.mimetype) this.mimetype = mime.types[path.extname(this.filename).slice(1)] || 'text/plain';
+var rootPath = path.join(__dirname, '..', '..');
 
-    var self = this;
-    fs.readFile(this.filename, function (error, data) {
-      if(error) return self.emit('error', error);
-      self.emit('created', {contents: data});
-    });
-  }
-});
-
-var assets = brocfile.inputTrees.map(function (t) {
-  return new StaticAsset({
-    filename: t.outputFile
-  , url: '/'+t.outputFile
+var rack = new rack.Rack([
+  new rack.StaticAssets({
+    urlPrefix: '/assets'
+  , dirname: rootPath + '/assets'
   , gzip: true
-  });
-});
-
-var rack = new rack.Rack(assets);
+  })
+]);
 
 module.exports = rack.handle;
