a79f86e9544c48bf6cc48567d4363074faa862aa
Moudy <moudy.elkammash@gmail.com>
2014-05-15 15:12:50 -0400

    add callback

--- config/db.js
+++ config/db.js
@@ -1,15 +1,17 @@
 var mongoose = require('mongoose');
 
-exports.connect = function (app) {
+exports.connect = function (app, done) {
   if ('development' === app.get('env')) mongoose.set('debug', true);
   var db = mongoose.connect(app.get('MONGO_URI')).connection;
 
   db.on('error', function () {
     console.log('MongoDB connection error', app.get('MONGO_URI'), arguments);
+    done && done();
   });
 
   db.on('open', function () {
     console.log('MongoDB connection open at:', app.get('MONGO_URI'));
+    done && done();
   });
 
   return db;
